4ae131731f504e01f119c4fa36f57e560ae3cda8
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2020-12-20 06:26:19 +0530

    small changes

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.query({
-		currentWindow: true,
-		active: true
-	}, function(tab) {
-		chrome.tabs.create({
-			"url": "http://dev.opera.com"
-		});
-	});
-});
 
 
 chrome.commands.onCommand.addListener(function(commandName) {
